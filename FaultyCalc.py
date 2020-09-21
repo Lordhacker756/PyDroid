8e2a680d4b7cbb788c1303c14b2ce9ddfc8989a4
Lordhacker756 <70888328+Lordhacker756@users.noreply.github.com>
2020-09-21 10:39:00 +0530

    FCalci
    A Calculator To Help You Fail Exams XD

---
+++ FaultyCalc.py
@@ -0,0 +1,21 @@
+print("Welcome To FCalc")
+print("Enter 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division")
+c = int(input())
+print("Enter Two Numbers")
+a=int(input())
+b=int(input())
+if a == 45 and b == 3 and c ==3:
+	print("Your Answer Is 555")
+if a == 56 and b == 9 and c ==1:
+	print("Your Answer Is 77")
+if a == 56 and b == 6 and c ==4:
+	print("Your Answer Is 4")
+elif c == 1:
+	print("Your Answer Is ", a+b)
+elif c == 2:
+	print("Your Answer Is ", a-b)
+elif c == 3:
+	print("Your Answer Is ", a*b)
+elif c == 4:
+	print("Your Answer Is ", a/b)
+print("Thanks XD")
